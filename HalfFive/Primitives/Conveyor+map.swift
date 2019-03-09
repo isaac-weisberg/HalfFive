dd889d7357d5b6f7d5fd9c177dd0542e3bff5a6e
Isaac Carol Weisberg <isaac_weisberg@outlook.com>
2019-03-09 20:58:55 +0700

    Remove the annoying named parameter in the map op

--- HalfFive/Primitives/Conveyor+map.swift
+++ HalfFive/Primitives/Conveyor+map.swift
@@ -1,5 +1,5 @@
 public extension ConveyorType {
-    func map<NewEvent>(predicate: @escaping (Event) -> NewEvent) -> Conveyor<NewEvent, Scheduler> {
+    func map<NewEvent>(_ predicate: @escaping (Event) -> NewEvent) -> Conveyor<NewEvent, Scheduler> {
         let run = self.run(handler:)
         return Conveyor<NewEvent, Scheduler> { handler in
             run { event in
