26ab24073ace7a071e1de22f8c2d1692a9ab4c3b
Isaac Carol Weisberg <isaac_weisberg@outlook.com>
2019-03-09 21:06:01 +0700

    Establish the behavior of the half five table data source

--- HalfFiveDataSource/HalfFiveTableDataSource.swift
+++ HalfFiveDataSource/HalfFiveTableDataSource.swift
@@ -1,17 +1,37 @@
 import HalfFive
 import UIKit
 
+
+
 public class HalfFiveTableDataSource<Event>: NSObject, UITableViewDataSource {
+    public typealias Sections = [HalfFiveSection<Event>]
+    
     public typealias Configurer = (UITableView, HalfFiveTableDataSource, IndexPath, Event) -> UITableViewCell
     
     let configurer: Configurer
     
-    public let sections = Container<[HalfFiveSection<Event>], SchedulingMain>(value: [])
+    public let sections = Container<Sections, SchedulingMain>(value: [])
+    
+    let newSections = Multiplexer<Sections, SchedulingMain>()
     
     public private(set) lazy var silo = sections.asSilo()
     
+    let trashBag = TrashBag()
+    
     public init(_ configurer: @escaping Configurer) {
         self.configurer = configurer
+        
+        super.init()
+        
+        newSections
+            .withLatest(from: sections) { (new: $0, old: $1) }
+            .do {[unowned self] stuff in
+                self.sections.fire(event: stuff.new)
+                let diff = PlainDiff(old: stuff.old, new: stuff.new)
+                // wait a second, how do I propagate the differentiated index paths...?
+            }
+            .run { _ in }
+            .disposed(by: trashBag)
     }
     
     public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
---
+++ HalfFiveDataSource/PlainDiff.swift
@@ -0,0 +1,5 @@
+import Foundation
+
+internal func PlainDiff<Event>(old: [HalfFiveSection<Event>], new: [HalfFiveSection<Event>]) -> [IndexPath] {
+    return []
+}
