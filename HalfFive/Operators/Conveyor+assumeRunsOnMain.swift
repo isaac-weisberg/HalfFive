82b2d5d1c6c4269c233d3e19409e1bc24d6faf7f
Isaac Weisberg <isaac_weisberg@outlook.com>
2019-04-12 14:33:12 +0700

    Delet the assume runs on main operator

--- HalfFive/Operators/Conveyor+assumeRunsOnMain.swift
+++
@@ -1,8 +0,0 @@
-//public extension SiloType where Hotness == SynchronisistyHot {
-//    func assumeRunsOnMain() -> Silo<Event, SchedulingMain> {
-//        let fire = self.fire(event:)
-//        return Silo { event in
-//            fire(event)
-//        }
-//    }
-//}
