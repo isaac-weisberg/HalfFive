ffe33fe147a5e3b1526d8c1d24c0c6d34fe6eee6
Isaac Weisberg <isaac_weisberg@outlook.com>
2019-04-14 12:51:15 +0700

    Establish a sample test model and sample test implementating data

--- Midesting/Models/TestGithubRemoteDTO.swift
+++ Midesting/Models/TestGithubRemoteDTO.swift
@@ -1,3 +1,28 @@
 class TestGithubRemoteDTO: Decodable {
+    struct QuestionDTO: Decodable {
+        struct Answer: Decodable {
+            let title: String
+        }
+        
+        enum Selection: Decodable {
+            case single
+            case multiple
+            
+            init(from decoder: Decoder) throws {
+                let container = try decoder.singleValueContainer()
+                let value = try container.decode(Int.self)
+                if value > 1 {
+                    self = .multiple
+                    return
+                }
+                self = .single
+            }
+        }
+        
+        let title: String
+        let answers: [Answer]
+        let selection: Selection
+    }
     
+    let questions: [QuestionDTO]
 }
