836cf881dc3bd334601f3bd5767bc76462e6aac2
Isaac Weisberg <isaac_weisberg@outlook.com>
2019-04-14 12:23:22 +0700

    Add test retrieval service to the app ctx

--- Midesting/Application/Context.swift
+++ Midesting/Application/Context.swift
@@ -6,12 +6,20 @@ protocol FileDownloaderServiceContext {
     var fileDownloader: FileDownloaderService { get }
 }
 
-typealias AppcontextProtocol = VoidServiceContext & FileDownloaderServiceContext
+protocol TestRetrievalServiceContext {
+    var testRetriever: TestRetrievalService { get }
+}
+
+typealias AppcontextProtocol = VoidServiceContext &
+    FileDownloaderServiceContext &
+    TestRetrievalServiceContext
 
 class AppContext: AppcontextProtocol {
+    let testRetriever: TestRetrievalService
     let fileDownloader: FileDownloaderService
     
     init() {
         fileDownloader = FileDownloaderServiceImpl()
+        testRetriever = TestRetrievalServiceImpl(fileDownloader: fileDownloader)
     }
 }
