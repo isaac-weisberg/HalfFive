fe2ed94e5731fdba076380a75b3f962561e8977e
Isaac Weisberg <isaac_weisberg@outlook.com>
2019-04-14 12:02:56 +0700

    Add more sense into the sync tests

--- HalfFiveTests/HotConveyorTest.swift
+++ HalfFiveTests/HotConveyorTest.swift
@@ -4,21 +4,38 @@ import XCTest
 class HotConveyorTest: XCTestCase {
     typealias C = Conveyors
     
-    let convs: [Conveyor<Void, SchedulingUnknown, HotnessHot>] = [
+    func didAllConveyorsEmitSynchronously<C: ConveyorType>(in conveyors: [C]) -> Bool {
+        return conveyors
+            .map { conveyor in
+                var emitted = false
+                _ = conveyor.run { _ in
+                    emitted = true
+                }
+                return emitted
+            }
+            .reduce(true) { $0 && $1 }
+    }
+    
+    let trivialConvs = [
         C.from(array: [(), (), ()]),
         C.sync { C.from(array: [(), ()]) },
         C.just(()),
+    ]
+    
+    func testTrivialConveyors() {
+        let result = didAllConveyorsEmitSynchronously(in: trivialConvs)
+        
+        XCTAssertTrue(result, "All of the trivial conveyors should've emmited synchronously upon subscription")
+    }
+    
+    let combinatorialConvs = [
         C.combineLatest(C.just(12), C.just(235)) { _, _ in () },
         C.zip(C.just(31), C.from(array: ["dic", "fuc"])) { _, _ in () },
     ]
-
-    func testHotConveyors() {
-        convs.forEach { conveyor in
-            var emitted = false
-            _ = conveyor.run {
-                emitted = true
-            }
-            XCTAssertTrue(emitted, "Should've been already executed")
-        }
+    
+    func testCombinatorialConvs() {
+        let result = didAllConveyorsEmitSynchronously(in: combinatorialConvs)
+        
+        XCTAssertTrue(result, "All of the combinatorial conveyors created from trivial conveyors should've emmited synchronously upon subscription")
     }
 }
