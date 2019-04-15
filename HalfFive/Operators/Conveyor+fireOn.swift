275e819e8462e15e0e95ba8517ac189e073f1321
Isaac Carol Weisberg <isaac_weisberg@outlook.com>
2019-04-15 23:37:12 +0700

    Make the fire on op also not too naive

--- HalfFive/Operators/Conveyor+fireOn.swift
+++ HalfFive/Operators/Conveyor+fireOn.swift
@@ -2,11 +2,17 @@ public extension ConveyorType {
     func fire<Scheduler: DeterminedScheduling>(on scheduler: Scheduler) -> Conveyor<Event, Scheduler, HotnessCold> {
         let run = self.run(handler:)
         return .init { handler in
-            run { event in
+            weak var weakTrash: Trash?
+            let trash = run { event in
                 scheduler.queue.async {
+                    guard weakTrash != nil else {
+                        return
+                    }
                     handler(event)
                 }
             }
+            weakTrash = trash
+            return trash
         }
     }
 }
