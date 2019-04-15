f9317b428bbecd6268da16c73532e822312f2ffd
Isaac Carol Weisberg <isaac_weisberg@outlook.com>
2019-04-15 23:53:13 +0700

    Realize that run on operators implementation is horribly leaky and wrong and attempt to rework it

--- HalfFive/Operators/Conveyor+runOn.swift
+++ HalfFive/Operators/Conveyor+runOn.swift
@@ -1,12 +1,21 @@
 public extension ConveyorType {
-    func run<RunScheduler: DeterminedScheduling>(on scheduler: RunScheduler) -> Conveyor<Event, Scheduler, HotnessCold> {
+    internal func runInternal<RunScheduler: DeterminedScheduling>(on scheduler: RunScheduler) -> Conveyor<Event, RunScheduler, HotnessCold> {
         let run = self.run(handler:)
         return Conveyor { handler in
+            weak var weakTrash: Trash?
             let trash = TrashDeferred()
             scheduler.queue.async {
+                guard weakTrash != nil else {
+                    return
+                }
                 trash.trash = run(handler)
             }
+            weakTrash = trash
             return trash
         }
+    }
+    
+    func run<RunScheduler: DeterminedScheduling>(on scheduler: RunScheduler) -> Conveyor<Event, RunScheduler, HotnessCold> where Hotness == HotnessHot {
+        return runInternal(on: scheduler)
     }
 }
