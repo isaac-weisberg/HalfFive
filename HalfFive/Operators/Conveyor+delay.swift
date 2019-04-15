bb51c52c48b2f7165bf5f3c4296787b00498d703
Isaac Carol Weisberg <isaac_weisberg@outlook.com>
2019-04-15 23:35:08 +0700

    Make the delay implementation a bit less naive

--- HalfFive/Operators/Conveyor+delay.swift
+++ HalfFive/Operators/Conveyor+delay.swift
@@ -4,11 +4,17 @@ public extension ConveyorType {
     func delay<Scheduler: DeterminedScheduling>(_ time: DispatchTime, on scheduler: Scheduler) -> Conveyor<Event, Scheduler, HotnessCold> {
         let run = self.run(handler:)
         return Conveyor { handler in
-            run { event in
+            weak var weakTrash: Trash?
+            let trash = run { event in
                 scheduler.queue.asyncAfter(deadline: time) {
+                    guard weakTrash != nil else {
+                        return
+                    }
                     handler(event)
                 }
             }
+            weakTrash = trash
+            return trash
         }
     }
 }
