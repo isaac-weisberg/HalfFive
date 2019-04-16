921a8db8fffb7c5b54708352865d7b868c338fa0
Isaac Weisberg <isaac_weisberg@outlook.com>
2019-04-16 13:06:05 +0700

    Omg this shit is so unstable, but this commit adds new tests

--- HalfFiveTests/ZipTests.swift
+++ HalfFiveTests/ZipTests.swift
@@ -4,49 +4,79 @@ import XCTest
 class ZipTests: XCTestCase {
     let trashBag = TrashBag()
     
-    func testZipCreationalOp() {
-        typealias Zip = [Int]
-        let exp = expectation(description: "Should gather all the results and produce consistent stuff")
+    typealias Zip = [Int]
+    let countBound = 225
+    let rangeBound = 226
+    
+    lazy var range = 0...Int.random(in: 1..<countBound)
+    
+    lazy var valuesOne = range.map { _ in Int.random(in: 0...rangeBound) }
+    lazy var valuesTwo = range.map { _ in Int.random(in: 0...rangeBound) }
+    lazy var one = Conveyors.from(array: valuesOne)
+    lazy var another = Conveyors.from(array: valuesTwo)
+    
+    lazy var expectedResults = { () -> [Zip] in
+        var res = [Zip]()
+        for (i, valueOne) in valuesOne.enumerated() {
+            let valueTwo = valuesTwo[i]
+            res.append([valueOne, valueTwo])
+        }
+        return res
+    }()
+    
+    func testZipCreationalOpSync() {
+        let exp = expectation(description: "Shoulda did all the ting")
         
-        let countBound = 25
-        let rangeBound = 26
+        checkZip(Conveyors.zip(one, another) { [$0, $1] }, exp: exp)
         
-        let range = 0...Int.random(in: 1..<countBound)
+        wait(for: [ exp ], timeout: 20)
+    }
+    
+    func testZipCreationalOpAsync() {
+        let exp = expectation(description: "Shoulda did all the ting")
         
-        let valuesOne = range.map { _ in Int.random(in: 0...rangeBound) }
-        let valuesTwo = range.map { _ in Int.random(in: 0...rangeBound) }
+        checkZip(Conveyors.zip(one.fire(on: SchedulingMain()), another.fire(on: SchedulingMain())) { [$0, $1] }, exp: exp)
         
-        assert(valuesOne.count == valuesTwo.count, "Fuck off")
+        wait(for: [ exp ], timeout: 20)
+    }
+    
+    func testZipCreationalOpAsyncBgFire() {
+        let exp = expectation(description: "Shoulda did all the ting")
         
-        let expectedResults = { () -> [Zip] in
-            var res = [Zip]()
-            for (i, valueOne) in valuesOne.enumerated() {
-                let valueTwo = valuesTwo[i]
-                res.append([valueOne, valueTwo])
-            }
-            return res
-        }()
+        checkZip(Conveyors.zip(one.fire(on: SchedulingSerial()), another.fire(on: SchedulingSerial())) { [$0, $1] }, exp: exp)
         
+        wait(for: [ exp ], timeout: 20)
+    }
+    
+    func testZipCreationalOpAsyncBgRun() {
+        let exp = expectation(description: "Shoulda did all the ting")
         
-        let one = Conveyors.from(array: valuesOne)
-            .fire(on: SchedulingSerial())
+        checkZip(Conveyors.zip(one.run(on: SchedulingSerial()), another.run(on: SchedulingSerial())) { [$0, $1] }, exp: exp)
         
-        let another = Conveyors.from(array: valuesTwo)
-            .fire(on: SchedulingSerial())
+        wait(for: [ exp ], timeout: 20)
+    }
+    
+    func testZipCreationalOpAsyncBgRunMainFire() {
+        let exp = expectation(description: "Shoulda did all the ting")
         
+        checkZip(Conveyors.zip(
+            one.run(on: SchedulingSerial()).fire(on: SchedulingMain()),
+            another.run(on: SchedulingSerial()).fire(on: SchedulingMain())
+        ) { [$0, $1] }, exp: exp)
+        
+        wait(for: [ exp ], timeout: 20)
+    }
+    
+    func checkZip<Scheduler: SchedulingTrait, Hotness: HotnessTrait>(_ zip: Conveyor<Zip, Scheduler, Hotness>, exp: XCTestExpectation) {
         var results = [Zip]()
-        
-        Conveyors
-            .zip(one, another) { [$0, $1] }
+        zip
             .run { tuple in
                 results.append(tuple)
-                if results.count == expectedResults.count {
-                    XCTAssertEqual(results, expectedResults, "Should be same")
+                if results.count == self.expectedResults.count {
+                    XCTAssertEqual(results, self.expectedResults, "Should be same")
                     exp.fulfill()
                 }
             }
             .disposed(by: trashBag)
-        
-        wait(for: [exp], timeout: 20)
     }
 }
