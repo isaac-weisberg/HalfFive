d38352299acfbc953f0a984bbcd5ddd9fa263bde
Isaac Weisberg <isaac_weisberg@outlook.com>
2019-04-16 15:44:22 +0700

    Swap the fire on operator impl to the one you can't cancel

--- HalfFive/Operators/Conveyor+fireOn.swift
+++ HalfFive/Operators/Conveyor+fireOn.swift
@@ -2,20 +2,11 @@ public extension ConveyorType {
     func fire<Scheduler: DeterminedScheduling>(on scheduler: Scheduler) -> Conveyor<Event, Scheduler, HotnessCold> {
         let run = self.run(handler:)
         return Conveyor { handler in
-            var `init` = false
-            weak var weakTrash: Trash?
-            let trash = run { event in
+            return run { event in
                 scheduler.queue.async {
-                    if !`init`, weakTrash == nil {
-                        return
-                    }
-                    
                     handler(event)
                 }
             }
-            `init` = true
-            weakTrash = trash
-            return trash
         }
     }
 }
