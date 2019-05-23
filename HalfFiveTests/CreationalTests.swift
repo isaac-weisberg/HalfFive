c2936e0f9bc7a7b2b3db01af63bffe15cc3c748f
Isaac Weisberg <isaac_weisberg@outlook.com>
2019-05-23 12:37:54 +0300

    Establish a pretty thorough testing for hot creational ops

---
+++ HalfFiveTests/CreationalTests.swift
@@ -0,0 +1,20 @@
+import XCTest
+@testable import HalfFive
+import HalfFiveTesting
+
+class CreationalTests: XCTestCase {
+    func blockingTest<Conveyor: ConveyorType>(_ blocking: Blocking<Conveyor>, events: [Conveyor.Event], file: StaticString = #file, line: UInt = #line) where Conveyor.Hotness == HotnessHot, Conveyor.Event: Equatable {
+        XCTAssertTrue(blocking.isHot(), "Should be hot", file: file, line: line);
+        XCTAssertTrue(blocking.proveHotness(), "Should prove it's type hotness", file: file, line: line)
+        XCTAssertEqual(blocking.hotEvents(), events, "Should have well defined event sequence", file: file, line: line)
+    }
+    
+    func testJustOperator() {
+        let value = 3
+        let conv = Conveyors.just(value)
+        
+        let blocking = conv.toBlocking()
+        
+        blockingTest(blocking, events: [ value ])
+    }
+}
