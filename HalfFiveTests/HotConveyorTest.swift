3005dd694b219518dad5ecc63ed9aa3274d6cb8f
Isaac Weisberg <isaac_weisberg@outlook.com>
2019-05-28 11:35:37 +0300

    Redo the hot conveyors test

--- HalfFiveTests/HotConveyorTest.swift
+++ HalfFiveTests/HotConveyorTest.swift
@@ -1,42 +1,22 @@
 import XCTest
 @testable import HalfFive
+import HalfFiveTesting
 
 class HotConveyorTest: XCTestCase {
     typealias C = Conveyors
     
-    func didAllConveyorsEmitSynchronously<C: ConveyorType>(in conveyors: [C]) -> Bool {
-        return conveyors
-            .map { conveyor in
-                var emitted = false
-                _ = conveyor.run { _ in
-                    emitted = true
-                }
-                return emitted
-            }
-            .reduce(true) { $0 && $1 }
-    }
-    
-    let trivialConvs = [
-        C.from(array: [(), (), ()]),
-        C.sync { C.from(array: [(), ()]) },
-        C.just(()),
-    ]
-    
-    func testTrivialConveyors() {
-        let result = didAllConveyorsEmitSynchronously(in: trivialConvs)
+    func testConveyorsHotness() {
+        let trivialConvs: [(Conveyor<Void, SchedulingUnknown, HotnessHot>, StaticString, UInt)] = [
+            (C.from(array: [(), (), ()]), #file, #line),
+            (C.sync { C.from(array: [(), ()]) }, #file, #line),
+            (C.just(()), #file, #line),
+            (C.combineLatest(C.just(12), C.just(235)) { _, _ in () }, #file, #line),
+            (C.zip(C.just(31), C.from(array: ["dic", "fuc"])) { _, _ in () }, #file, #line),
+            (C.just(3).flatMapLatest { _ in Conveyors.just(()) }, #file, #line)
+        ]
         
-        XCTAssertTrue(result, "All of the trivial conveyors should've emmited synchronously upon subscription")
-    }
-    
-    let combinatorialConvs = [
-        C.combineLatest(C.just(12), C.just(235)) { _, _ in () },
-        C.zip(C.just(31), C.from(array: ["dic", "fuc"])) { _, _ in () },
-        C.just(3).flatMapLatest { _ in Conveyors.just(()) }
-    ]
-    
-    func testCombinatorialConvs() {
-        let result = didAllConveyorsEmitSynchronously(in: combinatorialConvs)
-        
-        XCTAssertTrue(result, "All of the combinatorial conveyors created from trivial conveyors should've emmited synchronously upon subscription")
+        trivialConvs.forEach { stuff in
+            XCTAssertTrue(stuff.0.toBlocking().isHot(), "Should've been hot", file: stuff.1, line: stuff.2)
+        }
     }
 }
