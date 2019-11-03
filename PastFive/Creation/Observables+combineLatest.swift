6ef63ae397c270a6ebf5e02b03fae097239014e3
Isaac Weisberg <isaac_weisberg@outlook.com>
2019-11-03 18:35:05 +0300

    Add a single thread combineLatest implementation

---
+++ PastFive/Creation/Observables+combineLatest.swift
@@ -0,0 +1,55 @@
+public extension Observables {
+    static func combineLatest<Event, First: ObservableType, Second: ObservableType>
+        (_ first: First, second: Second, _ transform: @escaping (First.Event, Second.Event) -> Event)
+        -> Observable<Event, AsyncRunner<MainScheduler>>
+        where First.Scheduling == AsyncRunner<MainScheduler>, First.Scheduling == Second.Scheduling {
+
+        return Observable.unchecked { handler in
+            let disposable = CombinedDisposable<First.Event, Second.Event>()
+
+            disposable.first = first.subscribe { [weak disposable] event in
+                guard let disposable = disposable else {
+                    return
+                }
+                disposable.state.first = event
+                disposable.state.getEventsIfTheyAreThere { first, second in
+                    let newEvent = transform(first, second)
+                    handler(newEvent)
+                }
+            }
+
+            disposable.second = second.subscribe { [weak disposable] event in
+                guard let disposable = disposable else {
+                    return
+                }
+                disposable.state.second = event
+                disposable.state.getEventsIfTheyAreThere { first, second in
+                    let newEvent = transform(first, second)
+                    handler(newEvent)
+                }
+            }
+
+            return disposable
+        }
+    }
+}
+
+private class CombinedDisposable<First, Second>: Disposable {
+    var first: Disposable?
+    var second: Disposable?
+    var state = CombinedState<First, Second>()
+}
+
+private struct CombinedState<First, Second> {
+    var first: First?
+    var second: Second?
+
+    func getEventsIfTheyAreThere(_ getter: (First, Second) -> Void) {
+        guard
+            let first = first,
+            let second = second else {
+                return
+        }
+        getter(first, second)
+    }
+}
