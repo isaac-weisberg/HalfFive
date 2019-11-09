59409e6a76324831dbeec6479505bc35f7ddffaf
Isaac Weisberg <isaac_weisberg@outlook.com>
2019-11-09 17:45:50 +0300

    Rm an unneeded equals op

--- PastFive/Scheduling/DispatchQueueScheduler.swift
+++ PastFive/Scheduling/DispatchQueueScheduler.swift
@@ -1,10 +1,6 @@
 import Dispatch
 
 public struct DispatchQueueScheduler: KnownSchedulerType {
-    public static func == (lhs: Self, rhs: Self) -> Bool {
-        return false
-    }
-
     public let queue: DispatchQueue
 
     public init(queue: DispatchQueue) {
